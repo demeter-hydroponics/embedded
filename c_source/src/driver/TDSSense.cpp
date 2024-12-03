6c1532b6a90f89cd45643fe8d84c103a66b5ddd5
Sahil Kale <sahil.kale1@gmail.com>
2024-12-02 21:31:26 -0500

    Add Prelim TDS sensor

---
+++ c_source/src/driver/TDSSense.cpp
@@ -0,0 +1,40 @@
+#include "TDSSense.hpp"
+
+#include <cstdint>
+
+#include "HAL_ADC.hpp"
+
+TDSSense::TDSSense(HAL_ADC& adc, uint8_t channel, float gain, float offset)
+    : adc_(adc),
+      channel_(channel),
+      gain_(gain),
+      offset_(offset),
+      adc_error_(HAL_ADC::ErrorCode::INVALID_CHANNEL),
+      voltage_(0.0F) {}
+
+TDSSense::ErrorCode TDSSense::poll() {
+    TDSSense::ErrorCode error = TDSSense::ErrorCode::NO_ERROR;
+    float voltage = 0.0F;
+    const HAL_ADC::ErrorCode adc_error = adc_.readV(voltage, channel_);
+    if (adc_error != HAL_ADC::ErrorCode::NO_ERROR) {
+        error = TDSSense::ErrorCode::ADC_ERROR;
+    } else {
+        voltage_ = voltage;
+    }
+
+    adc_error_ = adc_error;
+
+    return error;
+}
+
+TDSSense::ErrorCode TDSSense::get_TDS_concentration(float& TDS) {
+    TDS = gain_ * voltage_ + offset_;
+
+    ErrorCode error = ErrorCode::NO_ERROR;
+
+    if (adc_error_ != HAL_ADC::ErrorCode::NO_ERROR) {
+        error = ErrorCode::ADC_ERROR;
+    }
+
+    return error;
+}
