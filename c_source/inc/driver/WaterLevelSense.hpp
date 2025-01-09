d91d8c865f3595f061037d9f56cdeba4df350ef4
Sahil Kale <sahil.kale1@gmail.com>
2025-01-09 15:13:57 -0500

    Add in Water level sensing

---
+++ c_source/inc/driver/WaterLevelSense.hpp
@@ -0,0 +1,35 @@
+#ifndef WATER_LEVEL_SENSE_HPP
+#define WATER_LEVEL_SENSE_HPP
+
+#include "TOF.hpp"
+
+class BaseWaterLevelSense {
+   public:
+    /**
+     * @brief Get the water level in the tank in liters
+     *
+     * @param water The water level in the tank in liters
+     * @return true if the water level was successfully read, false otherwise
+     */
+    virtual bool getWaterInTankL(float& water) = 0;
+};
+
+class WaterLevelSenseFromTOF : public BaseWaterLevelSense {
+   public:
+    WaterLevelSenseFromTOF(BaseTOF& tof, float scale, float offset);
+
+    void poll();
+
+    bool getWaterInTankL(float& water) override;
+
+   private:
+    BaseTOF& tof_;
+    float waterLevel_;
+
+    float scale_;
+    float offset_;
+
+    bool result_valid_ = false;
+};
+
+#endif  // WATER_LEVEL_SENSE_HPP
