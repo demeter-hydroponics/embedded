b930588e0fe43726e4031a12078219996fc8e938
Sahil Kale <sahil.kale1@gmail.com>
2025-02-22 17:56:04 -0500

    Add in gpio-based led

--- c_source/inc/device/StatusLED.hpp
+++ c_source/inc/device/StatusLED.hpp
@@ -1,6 +1,7 @@
 #ifndef STATUS_LED_HPP
 #define STATUS_LED_HPP
 
+#include "HAL_GPIO.hpp"
 #include "HAL_PWMTimer.hpp"
 
 class BaseStatusLED {
@@ -13,9 +14,37 @@ class BaseStatusLED {
     virtual ErrorCode setColour(float r, float g, float b) = 0;
 };
 
-class StatusLED : public BaseStatusLED {
+class StatusGPIOLED : public BaseStatusLED {
    public:
-    StatusLED(HAL_PWMTimer& pwmTimer, uint8_t pwmChannelR, uint8_t pwmChannelG, uint8_t pwmChannelB);
+    StatusGPIOLED(HAL_GPIO& pinR, HAL_GPIO& pinG, HAL_GPIO& pinB);
+
+    /**
+     * @brief Initialize the GPIO pins for the status LED
+     *
+     * @return Error code
+     */
+    ErrorCode init();
+
+    /**
+     * @brief Set the colour of the status LED
+     *
+     * @param r Red value (0-1)
+     * @param g Green value (0-1)
+     * @param b Blue value (0-1)
+     *
+     * @return Error code
+     */
+    ErrorCode setColour(float r, float g, float b) override;
+
+   private:
+    HAL_GPIO& pinR_;
+    HAL_GPIO& pinG_;
+    HAL_GPIO& pinB_;
+};
+
+class StatusPWMLED : public BaseStatusLED {
+   public:
+    StatusPWMLED(HAL_PWMTimer& pwmTimer, uint8_t pwmChannelR, uint8_t pwmChannelG, uint8_t pwmChannelB);
 
     /**
      * @brief Set the colour of the status LED
--- c_source/test/test_status_led.cpp
+++ c_source/test/test_status_led.cpp
@@ -1,3 +1,4 @@
+#include "MockGPIO.hpp"
 #include "Mock_PWMTimer.hpp"
 #include "StatusLED.hpp"
 
@@ -5,11 +6,38 @@ using namespace ::testing;
 
 TEST(StatusLEDTest, SetColour) {
     MockPWMTimer pwmTimer;
-    StatusLED statusLED(pwmTimer, 0, 1, 2);
+    StatusPWMLED StatusPWMLED(pwmTimer, 0, 1, 2);
 
     EXPECT_CALL(pwmTimer, setDutyCycle(0.5f, 0)).WillOnce(Return(HAL_PWMTimer::ErrorCode::NO_ERROR));
     EXPECT_CALL(pwmTimer, setDutyCycle(0.5f, 1)).WillOnce(Return(HAL_PWMTimer::ErrorCode::NO_ERROR));
     EXPECT_CALL(pwmTimer, setDutyCycle(0.5f, 2)).WillOnce(Return(HAL_PWMTimer::ErrorCode::NO_ERROR));
 
-    EXPECT_EQ(statusLED.setColour(0.5f, 0.5f, 0.5f), StatusLED::ErrorCode::NO_ERROR);
+    EXPECT_EQ(StatusPWMLED.setColour(0.5f, 0.5f, 0.5f), StatusPWMLED::ErrorCode::NO_ERROR);
+}
+
+TEST(StatusLEDGPIOTest, init) {
+    MockGPIO pinR;
+    MockGPIO pinG;
+    MockGPIO pinB;
+
+    testing::InSequence seq;
+
+    EXPECT_CALL(pinR, setPinMode(HAL_GPIO::PinMode::OUTPUT)).Times(1);
+    EXPECT_CALL(pinG, setPinMode(HAL_GPIO::PinMode::OUTPUT)).Times(1);
+    EXPECT_CALL(pinB, setPinMode(HAL_GPIO::PinMode::OUTPUT)).Times(1);
+
+    StatusGPIOLED statusLED(pinR, pinG, pinB);
+    statusLED.init();
+
+    // clear expectations
+    Mock::VerifyAndClearExpectations(&pinR);
+    Mock::VerifyAndClearExpectations(&pinG);
+    Mock::VerifyAndClearExpectations(&pinB);
+
+    // expect that the pins are set to true/false depending on duty cycle and return no error
+    EXPECT_CALL(pinR, writePin(false)).WillOnce(Return(HAL_GPIO::ErrorCode::NO_ERROR));
+    EXPECT_CALL(pinG, writePin(true)).WillOnce(Return(HAL_GPIO::ErrorCode::NO_ERROR));
+    EXPECT_CALL(pinB, writePin(true)).WillOnce(Return(HAL_GPIO::ErrorCode::NO_ERROR));
+
+    EXPECT_EQ(statusLED.setColour(0.0f, 1.0f, 1.0f), StatusGPIOLED::ErrorCode::NO_ERROR);
 }
