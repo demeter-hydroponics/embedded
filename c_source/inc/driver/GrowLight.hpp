33880ee4b6aa15999083f059e91672fc9a4d7f77
Sahil Kale <sahil.kale1@gmail.com>
2025-02-22 15:27:29 -0500

    Add in LED driver

---
+++ c_source/inc/driver/GrowLight.hpp
@@ -0,0 +1,55 @@
+#ifndef GROW_LIGHT_HPP
+#define GROW_LIGHT_HPP
+
+#include "HAL_ADC.hpp"
+#include "HAL_GPIO.hpp"
+#include "HAL_PWMChannel.hpp"
+
+class GrowLight {
+   public:
+    enum class ErrorCode {
+        NO_ERROR,
+        INVALID_OPERATION,
+        NOT_ENABLED,
+        HAL_ERROR,
+    };
+
+    /**
+     * @brief Construct a new Grow Light object
+     * @param pwmChannel The PWM channel to use for the grow light
+     * @param current_sense_adc The ADC to use for current sensing
+     * @param current_sense_adc_gain The gain of the current sense ADC
+     * @param adc_current_sense_channel The ADC channel to use for current sensing
+     */
+    GrowLight(HAL_PWMChannel& pwmChannel, HAL_ADC* current_sense_adc, float current_sense_adc_gain,
+              uint8_t adc_current_sense_channel);
+
+    /**
+     * @brief Initialize the grow light
+     * @brief The grow light frequency
+     * @return The error code
+     */
+    ErrorCode init(uint32_t growLightFreq);
+
+    /**
+     * @brief Set the grow light duty cycle
+     * @param dutyCycle The duty cycle to set the grow light to
+     * @return The error code
+     */
+    ErrorCode setDutyCycle(float dutyCycle);
+
+    /**
+     * @brief Get the current duty cycle of the grow light
+     * @param dutyCycle The current duty cycle of the grow light
+     * @return The error code
+     */
+    ErrorCode getCurrent(float& current);
+
+   private:
+    HAL_PWMChannel& pwmChannel_;
+    HAL_ADC* current_sense_adc_;
+    float current_sense_adc_gain_;
+    uint8_t adc_current_sense_channel_;
+};
+
+#endif  // GROW_LIGHT_HPP
---
+++ c_source/test/test_grow_light.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+
+#include "GrowLight.hpp"
+#include "MockADC.hpp"
+#include "Mock_PWMChannel.hpp"
+
+using namespace ::testing;
+
+TEST(GrowLightTest, SetDutyCycle) {
+    MockPWMChannel pwmChannel;
+    MockADC adc;
+    GrowLight growLight(pwmChannel, &adc, 1.0f, 1);
+
+    EXPECT_CALL(pwmChannel, setDutyCycle(0.5f)).WillOnce(testing::Return(HAL_PWMChannel::ErrorCode::NO_ERROR));
+
+    EXPECT_EQ(growLight.setDutyCycle(0.5f), GrowLight::ErrorCode::NO_ERROR);
+}
+
+TEST(GrowLightTest, SetDutyCycleInvalidOperation) {
+    MockPWMChannel pwmChannel;
+    GrowLight growLight(pwmChannel, nullptr, 1.0f, 1);
+
+    EXPECT_EQ(growLight.setDutyCycle(0.5f), GrowLight::ErrorCode::INVALID_OPERATION);
+}
+
+TEST(GrowLightTest, SetDutyCycleHalError) {
+    MockPWMChannel pwmChannel;
+    MockADC adc;
+    GrowLight growLight(pwmChannel, &adc, 1.0f, 1);
+
+    EXPECT_CALL(pwmChannel, setDutyCycle(0.5f)).WillOnce(testing::Return(HAL_PWMChannel::ErrorCode::HAL_ERROR));
+
+    EXPECT_EQ(growLight.setDutyCycle(0.5f), GrowLight::ErrorCode::HAL_ERROR);
+}
+
+TEST(GrowLightTest, init) {
+    MockPWMChannel pwmChannel;
+    MockADC adc;
+    GrowLight growLight(pwmChannel, &adc, 1.0f, 1);
+
+    EXPECT_CALL(pwmChannel, setFrequency(1000)).WillOnce(testing::Return(HAL_PWMChannel::ErrorCode::NO_ERROR));
+    EXPECT_EQ(growLight.init(1000), GrowLight::ErrorCode::NO_ERROR);
+
+    EXPECT_CALL(pwmChannel, setFrequency(1000)).WillOnce(testing::Return(HAL_PWMChannel::ErrorCode::HAL_ERROR));
+    EXPECT_EQ(growLight.init(1000), GrowLight::ErrorCode::HAL_ERROR);
+}
+
+TEST(GrowLightTest, getCurrent) {
+    MockADC adc;
+    MockPWMChannel pwmChannel;
+    GrowLight growLight(pwmChannel, &adc, 1.0f, 1);
+
+    EXPECT_CALL(adc, readV(_, 1)).WillOnce(DoAll(SetArgReferee<0>(0.5f), Return(HAL_ADC::ErrorCode::NO_ERROR)));
+    float current;
+    EXPECT_EQ(growLight.getCurrent(current), GrowLight::ErrorCode::NO_ERROR);
+    EXPECT_EQ(current, 0.5f);
+
+    EXPECT_CALL(adc, readV(_, 1)).WillOnce(DoAll(SetArgReferee<0>(0.5f), Return(HAL_ADC::ErrorCode::READ_ERROR)));
+    EXPECT_EQ(growLight.getCurrent(current), GrowLight::ErrorCode::HAL_ERROR);
+}
