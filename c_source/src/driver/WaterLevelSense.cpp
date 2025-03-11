09bce9c4ee537da84e02af26158b6269e989b21e
Sahil Kale <sahil.kale1@gmail.com>
2025-03-11 12:39:31 -0400

    Disable lpf for water level controller

--- c_source/src/driver/WaterLevelSense.cpp
+++ c_source/src/driver/WaterLevelSense.cpp
@@ -3,7 +3,7 @@
 WaterLevelSenseFromTOF::WaterLevelSenseFromTOF(BaseTOF& tof, float scale, float offset, float lpf_fc, float dt)
     : tof_(tof), scale_(scale), offset_(offset) {
     if ((lpf_fc > 0.0F) && (dt > 0.0F)) {
-        lpf_enabled_ = true;
+        lpf_enabled_ = false;
         control_utils_lpf_init(&lpf_, lpf_fc, dt);
     }
 }
@@ -12,17 +12,18 @@ void WaterLevelSenseFromTOF::poll() {
     float distance = 0.0F;
     if (tof_.get_distance_m(distance) == BaseTOF::ErrorCode::NO_ERROR) {
         rawWaterLevel_ = distance * scale_ + offset_;
+        waterLevel_ = rawWaterLevel_;
         if (lpf_enabled_) {
-            control_utils_lpf_step(&lpf_, rawWaterLevel_);
-            waterLevel_ = lpf_.output;
+            // control_utils_lpf_step(&lpf_, rawWaterLevel_);
+            // waterLevel_ = lpf_.output;
         } else {
-            waterLevel_ = rawWaterLevel_;
+            // waterLevel_ = rawWaterLevel_;
         }
         result_valid_ = true;
     } else {
         if (lpf_enabled_) {
-            control_utils_lpf_step(&lpf_, rawWaterLevel_);
-            waterLevel_ = lpf_.output;
+            // control_utils_lpf_step(&lpf_, rawWaterLevel_);
+            // waterLevel_ = lpf_.output;
         }
         result_valid_ = false;
     }
